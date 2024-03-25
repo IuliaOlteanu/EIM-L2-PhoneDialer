d4416018ce4932859375beb52a6b7027a16aeff5
IuliaOlteanu <iulia.olteanu@stud.acs.upb.ro>
2024-03-25 21:06:07 +0200

    phone dialer

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/phonedialer/Constants.java
@@ -0,0 +1,13 @@
+package lab03.eim.systems.cs.pub.phonedialer;
+
+public interface Constants {
+    final public static int Buttons [] = {
+
+            R.id.button1,
+            R.id.button2,
+            R.id.button3,
+
+    };
+    final public static int PERMISSION_REQUEST_CALL_PHONE = 1;
+    final public static int CONTACTS_MANAGER_REQUEST_CODE = 2;
+}
